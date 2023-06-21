35921351757b5fdfcd4984b7cd3fb57eedab372f
juancarlosparedesa1 <jcparedesa1@uce.edu.ec>
2023-06-21 18:57:49 -0500

    Props en Vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <Contador titulo="Estudiante" v-bind:inicio="4+2"/>
+  <Contador titulo="Profesor"  v-bind:inicio="101"/>
+  
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Contador from './components/Contador.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  },
-  components: {
-    HelloWorld
-  }
-  
+    Contador
 
+  },
   
 }
 </script>
---
+++ src/components/Contador.vue
@@ -0,0 +1,73 @@
+<template>
+  <h3>{{ tituloComponente || 'valor por defecto'}}</h3>
+  <h3>{{ valor2 }}</h3>
+  <p>{{ numero }} <sup>2</sup>={{obtenerCuadradoComputado}}</p>
+  <p>{{ numero }} <sup>2</sup>={{obtenerCuadradoComputado}}</p>
+  <p>{{ numero }} <sup>2</sup>={{obtenerCuadradoComputado}}</p>
+  <button type="button" v-on:click="suma()">+</button>
+  <button @:click="resta()">-</button>
+</template>
+  
+  <script>
+export default {
+  name: "Contador",
+  data() {
+    return {
+      numero: this.inicio,
+      tituloComponente:this.titulo
+    };
+  },
+  methods: {
+    obtenerCuadrado() {
+      console.log("Entro al metodo cuadrado");
+      return this.numero * this.numero;
+    },
+    suma(){
+        this.numero+=1;
+    },
+    resta(){
+        this.numero-=1;
+    }
+  },
+    computed:{
+        obtenerCuadradoComputado(){
+            console.log("Entro al metodo cuadrado computado")
+            return this.numero*this.numero
+        },
+
+        evaularTitulo(){
+          return this.tituloComponente|| 'valor por defecto'
+        }
+    },
+    props:{
+      titulo:String,
+      inicio:{
+        type:Number,
+        required:false,
+        default:100,
+        validator(value){
+          return value>100
+        }
+      }
+    }
+};
+</script>
+  
+  <style>
+    button{
+        background-color: #64B687;
+        border-radius: 5px;
+        border: 1px solid white;
+        color: white;
+        cursor: pointer;
+        margin: 0px 5px;
+        padding: 5px 15px;
+
+    }
+
+    button:hover{
+        background-color: #5aa67b;
+    
+    }
+</style>
+  
